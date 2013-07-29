bba8d1be90534d422e5888afa3ae65a1b9be0dab
Cherish <owensy.aiesec@gmail.com>
2013-07-29 11:08:38 -0700

    first commit

---
+++ ToDoList-Cherish/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList-Cherish
+//
+//  Created by Jinyao Xu on 7/29/13.
+//  Copyright (c) 2013 Jinyao Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList-CherishTests/ToDoList_CherishTests.m
@@ -0,0 +1,36 @@
+//
+//  ToDoList_CherishTests.m
+//  ToDoList-CherishTests
+//
+//  Created by Jinyao Xu on 7/29/13.
+//  Copyright (c) 2013 Jinyao Xu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToDoList_CherishTests : XCTestCase
+
+@end
+
+@implementation ToDoList_CherishTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
